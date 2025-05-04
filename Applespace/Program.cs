27560a2e69e8e04cd6ca4fcd3596d4264022b0ba
lightz <rodgercosta81@gmail.com>
2025-05-04 00:44:58 -0300

    Estilizando as paginas dos funcionarios e alterando a program para aceitar padroes PT-BR

--- Applespace/Program.cs
+++ Applespace/Program.cs
@@ -1,15 +1,14 @@
-using Applespace.Repositorio.Login;
+﻿using Applespace.Repositorio.Login;
 using Applespace.Repositorio.Produto;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
-
 builder.Services.AddScoped<ILoginRepositorio, LoginRepositorio>();
 
 var app = builder.Build();
@@ -19,14 +18,18 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// 👉 Aqui aplicamos a cultura pt-BR
+var cultureInfo = new CultureInfo("pt-BR");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
 app.Run();
