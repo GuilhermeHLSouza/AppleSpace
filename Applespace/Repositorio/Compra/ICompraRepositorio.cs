2025792d08feea054e2d1abca1d8ea7d5be3afdc
GuilhermeHLSouza <guilherme.souza726@etec.sp.gov.br>
2025-05-14 15:19:32 -0300

    começando a criar a parte da compra

---
+++ Applespace/Repositorio/Compra/ICompraRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using Applespace.Models;
+
+namespace Applespace.Repositorio.Compra
+{
+    public interface ICompraRepositorio
+    {
+        public void RegistroEndereço(int idCliente, int cep, int numero, string rua, string bairro, string complemento);
+        public void SelectUsuario(Clientes cliente);
+        public void FormaPgm();
+    }
+}
