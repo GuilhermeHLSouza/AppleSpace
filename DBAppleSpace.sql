818c35d5f544f9f668e1d5d59ba6a94284469b22
GuilhermeHLSouza <guilherme.souza726@etec.sp.gov.br>
2025-05-15 15:18:49 -0300

    subindo o banco

--- DBAppleSpace.sql
+++ DBAppleSpace.sql
@@ -1,90 +1,95 @@
-create database DbAppleSpace;
-use DbAppleSpace;
+CREATE DATABASE DbAppleSpace;
+USE DbAppleSpace;
 
-
+-- Tabela de Administradores
 CREATE TABLE Administradores (
-Id_Adm int PRIMARY KEY auto_increment,
-Telefone int,
-Nome varchar(50) NOT NULL,
-Senha varchar(25) NOT NULL,
-Email varchar(150) NOT NULL
+    Id_Adm INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(50) NOT NULL,
+    Telefone INT,
+    Email VARCHAR(150) NOT NULL,
+    Senha VARCHAR(25) NOT NULL
 );
 
-CREATE TABLE Enderecos (
-Id_Endereco smallint PRIMARY KEY auto_increment,
-CEP int NOT NULL,
-Numero tinyint NOT NULL,
-Rua varchar(75) NOT NULL,
-Bairro varchar(50) NOT NULL,
-Complemento varchar(50),
-Id_Cliente int
-);
-
-CREATE TABLE Transportadoras (
-Id_Tranportadora tinyint PRIMARY KEY auto_increment,
-Nome varchar(75) NOT NULL,
-Cnpj bigint NOT NULL,
-Telefone int,
-Email varchar(150) NOT NULL
-);
-
-CREATE TABLE Entregas (
-Id_Entrega smallint PRIMARY KEY auto_increment,
-Statu varchar(25) NOT NULL,
-Id_Tranportadora tinyint,
-Id_venda tinyint,
-Id_Endereco tinyint,
-FOREIGN KEY(Id_Tranportadora) REFERENCES Transportadoras (Id_Tranportadora)
-);
-
-CREATE TABLE Categoria (
-Id_Cate int PRIMARY KEY auto_increment,
-Nome_Cate varchar(50)
-);
-
-CREATE TABLE Venda (
-Id_Venda smallint PRIMARY KEY auto_increment,
-Forma_Pgm varchar(25) NOT NULL,
-Statu varchar(25) NOT NULL,
-Id_Carrinho int
-);
-
+-- Tabela de Clientes
 CREATE TABLE Clientes (
-Id_Cliente int PRIMARY KEY auto_increment,
-Nome  varchar(75) NOT NULL,
-Email varchar(150) NOT NULL,
-Cpf bigint NOT NULL,
-Telefone  int,
-Senha varchar(25) NOT NULL
+    Id_Cliente INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(75) NOT NULL,
+    Email VARCHAR(150) NOT NULL,
+    Cpf BIGINT NOT NULL,
+    Telefone INT,
+    Senha VARCHAR(25) NOT NULL
 );
 
+-- Tabela de Endereços
+CREATE TABLE Enderecos (
+    Id_Endereco SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    CEP INT NOT NULL,
+    Numero TINYINT NOT NULL,
+    Rua VARCHAR(75) NOT NULL,
+    Bairro VARCHAR(50) NOT NULL,
+    Complemento VARCHAR(50),
+    Id_Cliente INT,
+    FOREIGN KEY (Id_Cliente) REFERENCES Clientes(Id_Cliente)
+);
+
+-- Tabela de Transportadoras
+CREATE TABLE Transportadoras (
+    Id_Tranportadora TINYINT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(75) NOT NULL,
+    Cnpj BIGINT NOT NULL,
+    Telefone INT,
+    Email VARCHAR(150) NOT NULL
+);
+
+-- Tabela de Categorias
+CREATE TABLE Categoria (
+    Id_Cate INT PRIMARY KEY AUTO_INCREMENT,
+    Nome_Cate VARCHAR(50)
+);
+
+-- Tabela de Produtos
 CREATE TABLE Produtos (
-Cod_Barra int PRIMARY KEY auto_increment,
-Descricao varchar(250),
-Preco double(8,2) NOT NULL,
-Nome varchar(50) NOT NULL,
-Estoque smallint NOT NULL,
-Img varchar(500),
-Id_Adm int,
-Id_Cate int,
-FOREIGN KEY(Id_Adm) REFERENCES Administradores (Id_Adm),
-FOREIGN KEY(Id_Cate) REFERENCES Categoria (Id_Cate)
+    Cod_Barra INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(50) NOT NULL,
+    Descricao TEXT,
+    Preco DOUBLE(8,2) NOT NULL,
+    Estoque SMALLINT NOT NULL,
+    Img VARCHAR(500),
+    EmDestaque BOOLEAN NOT NULL DEFAULT FALSE,
+    Id_Adm INT,
+    Id_Cate INT,
+    FOREIGN KEY (Id_Adm) REFERENCES Administradores(Id_Adm),
+    FOREIGN KEY (Id_Cate) REFERENCES Categoria(Id_Cate)
 );
 
+-- Tabela de Carrinho
 CREATE TABLE Carrinho (
-Id_Carrinho int PRIMARY KEY auto_increment,
-Quantidade smallint NOT NULL,
-valor double(9,2) NOT NULL,
-Cod_Barra int,
-Id_Cliente int,
-FOREIGN KEY(Cod_Barra) REFERENCES Produtos (Cod_Barra),
-FOREIGN KEY(Id_Cliente) REFERENCES Clientes (Id_Cliente)
+    Id_Carrinho INT PRIMARY KEY AUTO_INCREMENT,
+    Quantidade SMALLINT NOT NULL,
+    Valor DOUBLE(9,2) NOT NULL,
+    Cod_Barra INT,
+    Id_Cliente INT,
+    FOREIGN KEY (Cod_Barra) REFERENCES Produtos(Cod_Barra),
+    FOREIGN KEY (Id_Cliente) REFERENCES Clientes(Id_Cliente)
 );
 
-ALTER TABLE Venda ADD FOREIGN KEY(Id_Carrinho) REFERENCES Carrinho (Id_Carrinho);
-ALTER TABLE Produtos MODIFY Descricao TEXT;
-ALTER TABLE Produtos ADD EmDestaque BOOLEAN NOT NULL DEFAULT FALSE; 
+-- Tabela de Vendas
+CREATE TABLE Venda (
+    Id_Venda SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    Forma_Pgm VARCHAR(25) NOT NULL,
+    Statu VARCHAR(25) NOT NULL,
+    Id_Carrinho INT,
+    FOREIGN KEY (Id_Carrinho) REFERENCES Carrinho(Id_Carrinho)
+);
 
-ALTER TABLE Entregas ADD FOREIGN KEY (Id_Venda) REFERENCES Venda(Id_Venda);
-ALTER TABLE Entregas ADD FOREIGN KEY (Id_Endereco) REFERENCES Endereos(Id_Endereco);
-
+-- Tabela de Entregas
+CREATE TABLE Entregas (
+    Id_Entrega SMALLINT PRIMARY KEY AUTO_INCREMENT,
+    Statu VARCHAR(25) NOT NULL,
+    Id_Tranportadora TINYINT,
+    Id_Venda SMALLINT,
+    Id_Endereco SMALLINT,
+    FOREIGN KEY (Id_Tranportadora) REFERENCES Transportadoras(Id_Tranportadora),
+    FOREIGN KEY (Id_Venda) REFERENCES Venda(Id_Venda),
+    FOREIGN KEY (Id_Endereco) REFERENCES Enderecos(Id_Endereco)
+);
